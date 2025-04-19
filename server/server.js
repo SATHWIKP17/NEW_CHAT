27ceb55f5e99f935622f66029be2225fc4fa1f3d
SATHWIKP17 <sathwikreddypulimamidi051708@gmail.com>
2025-04-19 10:14:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,25 +5,22 @@ const http=require('http');
 const {Server}=require('socket.io');
 const cors=require('cors');
 const server=http.createServer(app);
+const io=new Server(server,{cors:{
+    origin:['*'],
+    methods:['POST','GET']
+}});
 app.use(cors({
-  origin: 'https://new-chat-1-0c5o.onrender.com', // your frontend origin
-  methods: ['GET', 'POST']
-}));
-
-const io = new Server(server, {
-  cors: {
-    origin: 'https://new-chat-1-0c5o.onrender.com',
-    methods: ['GET', 'POST']
-  }
-});
+    origin:['*'],
+    methods:['POST','GET']
+}))
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"../client/build")));
 app.use(express.urlencoded({extended:true}));
 io.on("connection", (socket) => {
     console.log("🟢 A user connected");
 
-    socket.on("room", (ro) => {
-        let {room}=ro;
+    socket.on("romsg", (ro) => {
+        let room=ro.rom;
         socket.join(room);
     socket.on("msg", (data) => {
         socket.to(room).emit("msgg",data);
@@ -38,7 +35,7 @@ app.post("/",(req,res)=>{
       res.status(200).send("OK");
 });
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../client/build","index.html"));
+    res.sendFile(path.join(__dirname,"../client/build/index.html"));
 });
 const PORT=process.env.PORT|10000
 server.listen(PORT,"0.0.0.0",()=>{
