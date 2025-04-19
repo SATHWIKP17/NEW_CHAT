7638165f998e2e2a3018bb120670904e251f5c66
SATHWIKP17 <sathwikreddypulimamidi051708@gmail.com>
2025-04-19 10:12:24 +0530

    Update so.js

--- client/src/so.js
+++ client/src/so.js
@@ -1,99 +1,53 @@
-import React, { useState, useEffect ,useRef} from 'react';
+import React from 'react';
+import {useState,useEffect} from 'react';
+import {io} from 'socket.io-client';
 import {useLocation} from 'react-router-dom';
-import { io } from 'socket.io-client';
-import './App.css';
-const socket = io('https://new-chat-1-0c5o.onrender.com',{ transports: ["websocket", "polling"] });
-function Sh() {
+const socket=io("https://new-chat-1-0c5o.onrender.com");
+function Soc(){
     const locate=useLocation();
-    const room=locate.state;
-    const [mess, setMess] = useState([]);
-    const [input, setInput] = useState("");
-    const[sorted,setSorted]=useState([]);
-    const[r_mess,setR_mess]=useState([]);  
-    const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
-    function on(e){
-        const nn={text:input,time:new Date().toLocaleTimeString()}
-        setMess((prev)=>[...prev,nn]);
-        setInput("");
-        socket.emit("msg",(nn));
-    }
+    const rom=locate.state;
+    const [msg,setMsg]=useState([]);
+    const [text,setText]=useState("");
+    const [sorted,setSorted]=useState([]);
+    const[rmsg,setRmsg]=useState([]);
+    const [tmsg,setTmsg]=useState([]);
     function oon(e){
-        setInput(e.target.value);
-    }
-    let curr=useRef(null);
-    const [disp,setDisp]=useState(false);
-    useEffect(() => {
-        socket.emit('room',(room));
-        socket.on("msgg", (data) => {
-            setR_mess((prev) => [...prev, data]);
-        });
-    
-        return () => socket.off("msgg");
-        return()=>socket.off("room");
-    }, []); 
-    // useEffect(() => {
-    //     socket.on("mm", (msg) => {
-    //         setMess((prev) => [...prev, msg]);   
-    //     });
-
-    //     // Cleanup to prevent multiple bindings
-    //     return () => socket.off("mm");
-    // }, []);
-    // const msgg = [
-    //     {
-    //         m: "Hello This is Sathwik,Hope You Remembered Me"
-    //     },
-    //     {
-    //         m: "Hello This is Vignesh,Hope You Remembered Me"
-    //     },
-    //     {
-    //         m: "Hello This is Lokesh,Hope You Remembered Me"
-    //     }
-    // ]
-    const [sel,setSel]=useState(false);
-    const [ic,setIc]=useState("")
-    const [t,setT]=useState("");
-    function handlemd(){
-        setT(new Date());
-    }
-    function handlec(i){
-        setIc(i);
-        setSel(true);
-    }
-    function handlemup(){
-        let time=t-Date.now();
-        if(t>=-3000){
-            setDisp(true);
-        }
+        setText(e.target.value);
     }
     useEffect(()=>{
-        const a=[...mess,...r_mess];
+        socket.emit("romsg",rom);
+        socket.on("msgg",(data)=>{
+            setRmsg((prev)=>[...prev,data])});
+        return ()=>{
+            socket.off("msgg");
+            socket.off("romsg");
+        }
+    },[]);
+    function on(e){
+        e.preventDefault();
+        const tt={tex:text,time:new Date()};
+        socket.emit("msg",tt);
+        setMsg((prev)=>[...prev,tt]);
+        setTmsg((prev)=>[...prev,tt]);
+    }
+    useEffect(()=>{
+        const a=[...tmsg,...rmsg];
         a.sort((a,b)=>new Date(a.time)-new Date(b.time));
         setSorted(a);
-    },[mess,r_mess]);
-    useEffect(()=>{
-        if('Notification' in window){
-            Notification.requestPermission().then(permission=>{
-                console.log('Notification',permission);
-            })
-        }
-        if(Notification.permission=='granted'){
-            new Notification('title',r_mess[r_mess.length-1]);
-        }
-    },[r_mess]);
+    },[tmsg,rmsg]);
     return(
         <>
-            <div className="dd">
+         <div className="dd">
                 <header className="hh"><h1 className="hd">Let's Chat</h1></header>
                 <br/>
                 {sorted.map((i,index)=>(
-                    <div className={mess.includes(i)?"hh2":"hhhh"}><div className={mess.includes(i)?"hh3":"hhhh1"}>{i.text}<footer className="fff"><small><sub>{i.time}</sub></small></footer></div></div>))}
+                    <div className={tmsg.includes(i)?"hh2":"hhhh"}><div className={tmsg.includes(i)?"hh3":"hhhh1"}>{i.tex}<footer className="fff"><small><sub>{new Date(i.time).toLocaleTimeString()}</sub></small></footer></div></div>))}
                 <div className="iii">
-                    <input className="iiiii" type="text" name="input" value={input} onChange={oon}/>
+                    <input className="iiiii" type="text" name="input" onChange={oon}/>
                     <button className="bb" type="submit" onClick={on}>🚀</button>
                 </div>
             </div>
-        </> 
+        </>
     )
 }
-export default Sh;
+export default Soc;
